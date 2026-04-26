6b74cb9a3aea9b06d4cf20c0429ee8095ac619eb
Viktor <castesto@yandex.ru>
2026-04-27 01:19:26 +0300

    feat: create productCatalog page

---
+++ js/productCatalog.js
@@ -0,0 +1,49 @@
+const data = {
+  "products": [
+    {
+      "id": "1",
+      "image": "images/Кровать 5.png",
+      "type": "bed",
+      "name": "Гостинная модульная Lucido",
+      "price": "7159",
+      "sailPrice": "5017",
+    },
+    {
+      "id": "2",
+      "image": "images/Кровать 5.png",
+      "type": "bed",
+      "name": "Гостинная модульная Lucido",
+      "price": "7159",
+      "sailPrice": "5017",
+    },
+
+    {
+      "id": "3",
+      "image": "images/Кровать 5.png",
+      "type": "bed",
+      "name": "Гостинная модульная Lucido",
+      "price": "7159",
+      "sailPrice": "5017",
+    },
+
+    {
+      "id": "4",
+      "image": "images/Кровать 5.png",
+      "type": "bed",
+      "name": "Гостинная модульная Lucido",
+      "price": "7159",
+      "sailPrice": "5017",
+    },
+
+    {
+      "id": "5",
+      "image": "images/Кровать 5.png",
+      "type": "bed",
+      "name": "Гостинная модульная Lucido",
+      "price": "7159",
+      "sailPrice": "5017",
+    }
+  ]
+};
+
+console.log(data.products[0]);

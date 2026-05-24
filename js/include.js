d273bb786d12b45e92b6230d4bfd93e74e009fe9
Viktor <castesto@yandex.ru>
2026-05-24 14:17:22 +0300

    feat: add preloader

--- js/include.js
+++ js/include.js
@@ -18,4 +18,15 @@ async function includeComponents() {
     }
 }
 
+
+window.addEventListener('load', function() {
+    const preloader = document.getElementById('preloader');
+    if (preloader) {
+        preloader.style.opacity = '0';
+        setTimeout(function() {
+            preloader.style.display = 'none';
+        }, 300);
+    }
+});
+
 document.addEventListener('DOMContentLoaded', includeComponents);  
